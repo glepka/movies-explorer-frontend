92c256630b0fad4f050c87471f71dc76c137d7b3
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-19 19:16:10 +0300

    feat: сверстана секция NotFound

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NotFound.css';
+
+
+
+
+function NotFound() {
+    return(
+        <section className='not-found'>
+          <h2 className='not-found__title'>404</h2>
+          <p className='not-found__subtitle'>Страница не найдена</p>
+          <button className='not-found__button'>Назад</button>
+        </section>
+)
+}
+
+export default NotFound;
