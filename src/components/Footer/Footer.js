10893108ab353d30ba2a25294fd0e7456382cd83
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-19 19:13:55 +0300

    feat: сверстана секция Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer() {
+    return(
+       <footer className='footer'>
+        <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+       <div className='footer__link-container'>
+        <p className='footer__copyright'>&copy; 2023</p>
+        <div className='footer__links'>
+            <a className='footer__link' href='https://practicum.yandex.ru/' target='blank'>Яндекс.Практикум</a>
+            <a className='footer__link' href='https://github.com/glepka/' target='blank'>Github</a>
+        </div>
+       </div>
+       </footer>
+)
+}
+
+export default Footer;
