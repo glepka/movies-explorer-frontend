c0bdf9f0cf772db0807bf1a4d6389dbac09fc813
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-27 11:34:00 +0300

    feat: регистрация и логин

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import './Login.css';
 import Form from '../Form/Form';
+import useForm from '../UseForm/UseForm';
+import { EMAIL_REGEX } from '../../utils/constants';
 
 
+function NotFound({ onAuthorize, isLoading }) {
+  const { enteredValues, errors, handleChange, isFormValid } = useForm();
 
-function NotFound() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAuthorize({
+      email: enteredValues.email,
+      password: enteredValues.password,
+    });
+  }
     return(
         <Form
         title="Рады видеть!"
         buttonText="Войти"
         question="Еще не зарегистрированы?"
         linkText=" Регистрация"
-        link="/signup">
+        link="/signup"
+        onSubmit={handleSubmit}
+        isDisabled={!isFormValid}
+        isLoading={isLoading}>
         <label className="form__field">
           E-mail
-          <input name="email" className="form__input" id="email-input" type="text" required />
-          <span className="form__input-error">Что-то пошло не так...</span>
+          <input
+          name="email"
+          className="form__input"
+          id="email-input"
+          type="text"
+          required
+          onChange={handleChange}
+          pattern={EMAIL_REGEX}
+          value={enteredValues.email || ''}
+          />
+          <span className="form__input-error">{errors.email}</span>
         </label>
         <label className="form__field">
           Пароль
-          <input name="password" className="form__input" id="password-input" type="password" />
-          <span className="form__input-error">Что-то пошло не так...</span>
+          <input
+          name="password"
+          className="form__input"
+          id="password-input"
+          type="password"
+          onChange={handleChange}
+          value={enteredValues.password || ''}/>
+          <span className="form__input-error">{errors.password}</span>
         </label>
       </Form>
 )
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import '../Form/Form.css';
 import Form from '../Form/Form';
+import useForm from '../UseForm/UseForm';
+import { EMAIL_REGEX, USER_NAME_REGEX } from '../../utils/constants';
 
-function Register() {
+function Register({ onRegister, isLoading }) {
+
+  const { enteredValues, errors, handleChange, isFormValid } = useForm();
+
+  function handleSubmit(e) {
+    console.log(enteredValues)
+    e.preventDefault();
+    onRegister({
+      name: enteredValues.name,
+      email: enteredValues.email,
+      password: enteredValues.password,
+    });
+  }
   return (
     <Form
       title="Добро пожаловать!"
       buttonText="Зарегистрироваться"
       question="Уже зарегистрированы?"
       linkText=" Войти"
-      link="/signin">
+      link="/signin"
+      onSubmit={handleSubmit}
+      isDisabled={!isFormValid}
+      isLoading={isLoading}>
       <label className="form__field">
         Имя
         <input
@@ -20,18 +37,37 @@ function Register() {
           minLength="2"
           maxLength="40"
           required
+          onChange={handleChange}
+          value={enteredValues.name || ''}
+          pattern={USER_NAME_REGEX}
         />
-        <span className="form__input-error">Что-то пошло не так...</span>
+        <span className="form__input-error">{errors.name}</span>
       </label>
       <label className="form__field">
         E-mail
-        <input name="email" className="form__input" id="email-input" type="text" required />
-        <span className="form__input-error">Что-то пошло не так...</span>
+        <input
+        name="email"
+        className="form__input"
+        id="email-input"
+        type="text"
+        required
+        onChange={handleChange}
+        pattern={EMAIL_REGEX}
+        value={enteredValues.email || ''}
+        />
+        <span className="form__input-error">{errors.email}</span>
       </label>
       <label className="form__field">
         Пароль
-        <input name="password" className="form__input" id="password-input" type="password" />
-        <span className="form__input-error">Что-то пошло не так...</span>
+        <input
+        name="password"
+        className="form__input"
+        id="password-input"
+        type="password"
+        onChange={handleChange}
+        value={enteredValues.password || ''}
+        />
+        <span className="form__input-error">{errors.password}</span>
       </label>
     </Form>
   );
