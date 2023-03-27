89a0358685c139e8fe7e17d6f29f1e9131111eff
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-27 11:42:54 +0300

    feat: ссылка в not found

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,7 +10,9 @@ function NotFound() {
         <section className='not-found'>
           <h2 className='not-found__title'>404</h2>
           <p className='not-found__subtitle'>Страница не найдена</p>
-          <button className='not-found__button'>Назад</button>
+          <Link to="/" className="not-found__button">
+            Назад
+          </Link>
         </section>
 )
 }
