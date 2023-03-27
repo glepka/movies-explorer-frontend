f67c68575725003a0a8bde4ad3f2fe0db9437925
Gleb Lyashenkov <glepka17@yandex.ru>
2023-03-27 11:37:33 +0300

    feat: отображение карточек с фильмами

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,21 +1,132 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import Header from '../Header/Header';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
+import { filterMovies, filterDuration } from '../../utils/utils';
+
+import * as movies from '../../utils/MoviesApi';
 
 
+function Movies({loggedIn, handleLikeClick, savedMovies, onCardDelete}) {
 
-function Movies() {
+  const [isLoading, setIsLoading] = useState(false); //загрузка прелоадер
+  const [initialMovies, setInitialMovies] = useState([]); //отфильтрованные по запросу
+  const [filteredMovies, setFilteredMovies] = useState([]); //отфильтрованные по запросу и чекбоксу
+  const [isShortMovies, setIsShortMovies] = useState(false); //включен ли чекбокс короткометражек
+
+
+  const [isReqErr, setIsReqErr] = useState(false); //ошибка запроса к серверу
+  const [isNotFound, setIsNotFound] = useState(false); //фильмы по запросу не найдены
+
+
+//основнай метод фильрации, который отдает массив с фильмами на рендеринг
+  function handleFilterMovies(movies, query, short) {
+  console.log(short);
+  const moviesList = filterMovies(movies, query, short); //фильтруем полученный массив по запросу
+  console.log(moviesList);
+  setInitialMovies(moviesList); //записываем в стейт
+  setFilteredMovies(short ? filterDuration(moviesList) : moviesList); //если чекбокс тру, то фильруем по длине и записываем в стейт
+  localStorage.setItem('movies', JSON.stringify(moviesList));
+  localStorage.setItem('allMovies', JSON.stringify(movies));
+}
+
+
+function handleShortMovies() {
+  setIsShortMovies(!isShortMovies);
+  if (!isShortMovies) {
+    if (filterDuration(initialMovies).length === 0) {
+      setFilteredMovies(filterDuration(initialMovies));
+    } else {
+      setFilteredMovies(filterDuration(initialMovies));
+    }
+  } else {
+    setFilteredMovies(initialMovies);
+  }
+  localStorage.setItem('shortMovies', !isShortMovies);
+}
+
+ //submit
+ function onSearchMovies(query) {
+  console.log(query);
+
+  localStorage.setItem('movieSearch', query);
+  localStorage.setItem('shortMovies', isShortMovies);
+
+  if (localStorage.getItem('allMovies')) {
+    const movies = JSON.parse(localStorage.getItem('allMovies'));
+    handleFilterMovies(movies, query, isShortMovies);
+  } else {
+    setIsLoading(true);
+    movies
+      .getCards()
+      .then((cardsData) => {
+        handleFilterMovies(cardsData, query, isShortMovies);
+        setIsReqErr(false);
+      })
+      .catch((err) => {
+        setIsReqErr(true);
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+}
+
+useEffect(() => {
+  if (localStorage.getItem('shortMovies') === 'true') {
+    setIsShortMovies(true);
+  } else {
+    setIsShortMovies(false);
+  }
+}, []);
+
+useEffect(() => {
+  if (localStorage.getItem('movies')) {
+    const movies = JSON.parse(localStorage.getItem('movies'));
+    setInitialMovies(movies);
+    if (localStorage.getItem('shortMovies') === 'true') {
+      setFilteredMovies(filterDuration(movies));
+    } else {
+      setFilteredMovies(movies);
+    }
+  }
+}, []);
+
+useEffect(() => {
+  if (localStorage.getItem('movieSearch')) {
+    if (filteredMovies.length === 0) {
+      setIsNotFound(true);
+    } else {
+      setIsNotFound(false);
+    }
+  } else {
+    setIsNotFound(false);
+  }
+}, [filteredMovies]);
+
 
     return(
         <>
-        <Header />
+        <Header loggedIn={loggedIn}/>
         <main>
         <section className='movies'>
-        <SearchForm />
-        <MoviesCardList />
+        <SearchForm
+        onSearchMovies={onSearchMovies}
+        onFilter={handleShortMovies}
+        isShortMovies={isShortMovies}/>
+        <MoviesCardList
+        savedMovies={savedMovies}
+        cards={filteredMovies}
+        isSavedFilms={false}
+        isLoading={isLoading}
+        isReqErr={isReqErr}
+        isNotFound={isNotFound}
+        handleLikeClick={handleLikeClick}
+        onCardDelete={onCardDelete}/>
         </section>
         </main>
         <Footer />
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import './MoviesCard.css';
-import cardImage from '../../images/card-image.jpg';
+import { durationConverter } from '../../utils/utils';
 
 
 
 
 
-function MoviesCard() {
+function MoviesCard({ card, isSavedFilms, handleLikeClick, onCardDelete, saved, savedMovies }) {
+
+
+
+  function onCardClick() {
+    console.log(saved)
+    if (saved) {
+      onCardDelete(savedMovies.filter((m) => m.movieId === card.id)[0]);
+    } else {
+      handleLikeClick(card);
+    }
+  }
+
+  function onDelete() {
+    onCardDelete(card);
+  }
+
+  const cardSaveButtonClassName = `${
+    saved ? 'card__button card__button_type_active' : 'card__button'
+  }`;
 
 
     return(
 
         <li className='card'>
-            <img className='card__image' src={cardImage} alt='Изображение карточки'/>
+              <a href={card.trailerLink} target="_blank" rel="noreferrer">
+                <img
+                  className="card__image"
+                  alt={card.nameRU}
+                  src={isSavedFilms ? card.image : `https://api.nomoreparties.co/${card.image.url}`}
+                />
+              </a>
             <div className='card__description'>
-                <h2 className='card__name'>33 слова о дизайне</h2>
-                <button className='card__like card__like_type_delete'>
-                    <></>
-                </button>
+                <h2 className='card__name'>{card.nameRU}</h2>
+                {isSavedFilms ? (
+                    <button type="button" className="card__button card__button_type_delete" onClick={onDelete}></button>
+                  ) : (
+                    <button type="button" className={cardSaveButtonClassName} onClick={onCardClick}></button>
+                  )}
             </div>
-            <span className='card__duration'>1ч 42м</span>
+            <span className='card__duration'>{durationConverter(card.duration)}</span>
         </li>
     )
 }
